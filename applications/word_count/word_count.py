9dec57ccf9d3aa0cde9def253971e0e68cd62b90
JonathanChen <jonny29009@yahoo.com>
2020-06-01 16:50:18 -0700

    finish word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,25 @@
-def word_count(s):
-    # Your code here
+specials = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
 
+
+def replace_special(main, toBeReplaces, newString):
+    for elem in toBeReplaces:
+        if elem in main:
+            main = main.replace(elem, newString)
+    return main
+
+
+def word_count(s):
+    count = {}
+    new_s = replace_special(s, specials, '')
+    new_s = new_s.split()
+    for i in range(len(new_s)):
+        new_s[i] = new_s[i].lower()
+        if new_s[i] in count:
+            count[new_s[i]] += 1
+        elif new_s[i] != '':
+            count[new_s[i]] = 1
+    return count
 
 
 if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
