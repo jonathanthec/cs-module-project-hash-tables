99b35855f2feb642e6a9f702e551c12b97dd1a3b
JonathanChen <jonny29009@yahoo.com>
2020-06-02 14:03:08 -0700

    finish histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,48 @@
-# Your code here
+def replace_special(main, replaced, new_words):
+    for elem in replaced:
+        if elem in main:
+            main = main.replace(elem, new_words)
+    return main
 
+
+def process_input(file):
+    specials = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '!', '?']
+    with open(file) as f:
+        words = f.read()
+        words = replace_special(words, specials, '')
+        words = words.lower()
+        words = words.split()
+        words = sorted(words)
+    return words
+
+
+def histogram(file):
+    words = process_input(file)
+    dictionary = {}
+
+    for word in words:
+        if word in dictionary:
+            dictionary[word] += '#'
+        else:
+            dictionary[word] = '#'
+
+    return sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+
+
+def print_histogram(dictionary):
+    max_len = 0
+    for item in dictionary:
+        max_len = len(item[0]) if len(item[0]) > max_len else max_len
+
+    for item in dictionary:
+        result = ''
+        result += item[0]
+        for i in range(max_len - len(item[0]) + 2):
+            result += ' '
+        result += item[1]
+        print(result)
+
+
+if __name__ == '__main__':
+    sorted_list = histogram('robin.txt')
+    print_histogram(sorted_list)
